881c5d2e632039d37be3e31fffc624c98c6fe07a
Nasim Attar <attarnasim8@gmail.com>
2024-06-26 23:55:40 +0200

    Project 6 improvements

--- project/pipeline.py
+++ project/pipeline.py
@@ -3,7 +3,7 @@ import sqlite3
 import pandas as pd
 from kaggle.api.kaggle_api_extended import KaggleApi
 
-def main():
+def download_and_process_data():
     # Dataset is pulled from Kaggle, the credentials need to be provided.
     api = KaggleApi()
     api.authenticate()
@@ -12,23 +12,27 @@ def main():
     dataset2 = 'ulrikthygepedersen/co2-emissions-by-country'
 
     data_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data')
-    response_csv1 = api.dataset_download_files(dataset1, path=data_dir, unzip=True)
-    response_csv2 = api.dataset_download_files(dataset2, path=data_dir, unzip=True)
+    os.makedirs(data_dir, exist_ok=True)
+    
+    print(f"Downloading datasets to {data_dir}")
+    api.dataset_download_files(dataset1, path=data_dir, unzip=True)
+    api.dataset_download_files(dataset2, path=data_dir, unzip=True)
     print("CSV files downloaded and unzipped successfully. Converting to a database...")
+    
     csv_file1 = os.path.join(data_dir, 'annual-change-forest-area.csv')
     csv_file2 = os.path.join(data_dir, 'co2_emissions_kt_by_country.csv')
 
     try:
         pd_csv1 = pd.read_csv(csv_file1)
         pd_csv2 = pd.read_csv(csv_file2)
-        os.makedirs(os.path.dirname("../data/pipeline.db"), exist_ok=True)
-        connection = sqlite3.connect("../data/pipeline.db")
+        db_path = os.path.join(data_dir, "pipeline.db")
+        connection = sqlite3.connect(db_path)
         pd_csv1.to_sql("deforestation", connection, if_exists="replace", index=False)
         pd_csv2.to_sql("co2", connection, if_exists="replace", index=False)
         connection.close()
-        print("Database saved in 'data' directory successfully.")
+        print(f"Database saved at {db_path} successfully.")
     except Exception as e:
         print(f"Error: {e}")
 
 if __name__ == "__main__":
-    main()
+    download_and_process_data()
--- project/tests.py
+++ project/tests.py
@@ -1,59 +1,53 @@
 import os
-import sys
-# Add the parent directory to sys.path
-current_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.dirname(current_dir)
-sys.path.insert(0, parent_dir)
 import unittest
-import importlib
 import pandas as pd
 import sqlite3 
-import pipeline
+from pipeline import download_and_process_data
 
 data_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data')
 
 class TestPipelineComponents(unittest.TestCase):
   
-  @classmethod
-  def setUpClass(cls):
-    cls.deforestation_file_path = os.path.join(data_dir, 'annual-change-forest-area.csv')
-    cls.co2_file_path = os.path.join(data_dir, 'co2_emissions_kt_by_country.csv')
-    cls.expected_deforestation_columns = ['Entity', 'Code', 'Year', 'Net forest conversion']
-    cls.expected_co2_columns = ['country_code', 'country_name', 'year', 'value']
-    cls.dbname = "pipeline"
-    cls.dbpath = os.path.join(data_dir, f"{cls.dbname}.db")
+    @classmethod
+    def setUpClass(cls):
+        print("Downloading and processing data...")
+        download_and_process_data()
+        cls.deforestation_file_path = os.path.join(data_dir, 'annual-change-forest-area.csv')
+        cls.co2_file_path = os.path.join(data_dir, 'co2_emissions_kt_by_country.csv')
+        cls.expected_deforestation_columns = ['Entity', 'Code', 'Year', 'Net forest conversion']
+        cls.expected_co2_columns = ['country_code', 'country_name', 'year', 'value']
+        cls.dbname = "pipeline"
+        cls.dbpath = os.path.join(data_dir, f"{cls.dbname}.db")
 
-  @classmethod
-  def tearDownClass(cls):
-    if os.path.exists(cls.dbpath):
-      os.remove(cls.dbpath)
+    @classmethod
+    def tearDownClass(cls):
+        if os.path.exists(cls.dbpath):
+            os.remove(cls.dbpath)
 
-  def test_data_extraction(self):
-    deforestation_data = pd.read_csv(self.deforestation_file_path)
-    co2_data = pd.read_csv(self.co2_file_path)
+    def test_data_extraction(self):
+        deforestation_data = pd.read_csv(self.deforestation_file_path)
+        co2_data = pd.read_csv(self.co2_file_path)
 
-    self.assertFalse(deforestation_data.empty, msg="Deforestation data should not be empty")
-    self.assertFalse(co2_data.empty, msg="CO2 emissions data should not be empty")
-    self.assertListEqual(list(deforestation_data.columns), self.expected_deforestation_columns,
-               msg="Deforestation data columns do not match expected columns")
-    self.assertListEqual(list(co2_data.columns), self.expected_co2_columns,
-               msg="CO2 emissions data columns do not match expected columns")
+        self.assertFalse(deforestation_data.empty, msg="Deforestation data should not be empty")
+        self.assertFalse(co2_data.empty, msg="CO2 emissions data should not be empty")
+        self.assertListEqual(list(deforestation_data.columns), self.expected_deforestation_columns,
+                   msg="Deforestation data columns do not match expected columns")
+        self.assertListEqual(list(co2_data.columns), self.expected_co2_columns,
+                   msg="CO2 emissions data columns do not match expected columns")
 
-  def test_database_integrity(self):
-    pipeline.main()
+    def test_database_integrity(self):
+        self.assertTrue(os.path.exists(self.dbpath), msg="Database file should exist after running the pipeline.")
+        
+        with sqlite3.connect(self.dbpath) as conn:
+            deforestation_table = pd.read_sql_query("SELECT * FROM deforestation", conn)
+            co2_table = pd.read_sql_query("SELECT * FROM co2", conn)
 
-    self.assertTrue(os.path.exists(self.dbpath), msg="Database file should exist after running the pipeline.")
-    
-    with sqlite3.connect(self.dbpath) as conn:
-      deforestation_table = pd.read_sql_query("SELECT * FROM deforestation", conn)
-      co2_table = pd.read_sql_query("SELECT * FROM co2", conn)
-
-      self.assertFalse(deforestation_table.empty, msg="The deforestation table should not be empty in the database")
-      self.assertFalse(co2_table.empty, msg="The co2 table should not be empty in the database")
-      self.assertListEqual(list(deforestation_table.columns), self.expected_deforestation_columns,
-                 msg="The columns in the deforestation table do not match the expected columns")
-      self.assertListEqual(list(co2_table.columns), self.expected_co2_columns,
-                 msg="The columns in the co2 table do not match the expected columns")
+            self.assertFalse(deforestation_table.empty, msg="The deforestation table should not be empty in the database")
+            self.assertFalse(co2_table.empty, msg="The co2 table should not be empty in the database")
+            self.assertListEqual(list(deforestation_table.columns), self.expected_deforestation_columns,
+                        msg="The columns in the deforestation table do not match the expected columns")
+            self.assertListEqual(list(co2_table.columns), self.expected_co2_columns,
+                        msg="The columns in the co2 table do not match the expected columns")
 
 if __name__ == "__main__":
-  unittest.main()
+    unittest.main()
